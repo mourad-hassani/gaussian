a4e4e6258699ca69d0c1d5daf3cd481bdac7402c
Mourad Hassani <mouradhassani.9101@gmail.com>
2024-06-22 17:12:06 +0200

    Make DTYPE a torch.dtype with value torch.float16

--- parameters.py
+++ parameters.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+import torch
 
 MODEL_NAME: str = "prajjwal1/bert-small"
 
@@ -12,7 +13,7 @@ EPOCHS: int = 1
 NUM_WARMUP_RATIO: float = 0.1
 MAX_SEQ_LEN: int = 64
 DEVICE: str = "cuda:0"
-DTYPE: str = "f16"
+DTYPE: torch.dtype = torch.float16
 SEED: int = 0
 TEMPERATURE: float = 0.05
 NUM_EVAL_STEPS: int = 100
