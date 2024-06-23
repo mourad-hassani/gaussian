cf6b385f8d5fb686ca50673780337569b731d290
Mourad Hassani <mouradhassani.9101@gmail.com>
2024-06-23 15:34:48 +0200

    Add DEVICE parameter to inference.py file

--- inference.py
+++ inference.py
@@ -1,11 +1,11 @@
 from gauss_model import GaussModel
-from parameters import MODEL_NAME
+from parameters import MODEL_NAME, DEVICE
 import torch
 from utils.evaluator import Evaluator
 from execution import Execution
 
-model: GaussModel = GaussModel(MODEL_NAME, True).eval().to("mps")
-model.load_state_dict(torch.load('temporal_bert.pth', map_location=torch.device("mps")))
+model: GaussModel = GaussModel(MODEL_NAME, True).eval().to(DEVICE)
+model.load_state_dict(torch.load('temporal_bert.pth', map_location=torch.device(DEVICE)))
 execution = Execution()
 
 evaluator = Evaluator(sim_fn=execution.sim_fn)
